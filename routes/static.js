1ba0c56c798da4a2b15242f66bf7146d2e9d2bef
Jonah West <131692115+jonahhwest0408@users.noreply.github.com>
2024-09-16 12:07:50 -0700

    Initial commit/ setup

--- routes/static.js
+++ routes/static.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
+router.use("/css", express.static(__dirname + "public/css")); // any route that contains /css is to refer to the public/css folder, which is found at the root level of the project.
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
