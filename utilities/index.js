6a5a579b34a458f9cec68d52a16f79845e68f026
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-10-01 13:19:34 -0500

    Finished styling for PDP pages

--- utilities/index.js
+++ utilities/index.js
@@ -27,35 +27,34 @@ Util.getNav = async function (req, res, next) {
 /* **************************************
 * Build the classification view HTML
 * ************************************ */
-Util.buildClassificationGrid = async function(data){
-    let grid
-    if(data.length > 0){
-      grid = '<ul id="inv-display">'
-      data.forEach(vehicle => { 
-        grid += '<li>'
-        grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-        + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-        + 'details"><img src="' + vehicle.inv_thumbnail 
-        +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-        +' on CSE Motors" /></a>'
-        grid += '<div class="namePrice">'
-        grid += '<hr />'
-        grid += '<h2>'
-        grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-        + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-        grid += '</h2>'
-        grid += '<span>$' 
-        + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-        grid += '</div>'
-        grid += '</li>'
-      })
-      grid += '</ul>'
-    } else { 
-      grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+Util.buildClassificationGrid = async function (data) {
+    let grid;
+    if (data.length > 0) {
+        grid = '<div id="inv-display">'; // Change from <ul> to <div>
+        data.forEach(vehicle => {
+            grid += '<div class="product-card">'; // Create a card for each vehicle
+            grid += '<a href="../../inv/detail/' + vehicle.inv_id + 
+                     '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model + 
+                     ' details" class="card-image"><img src="' + vehicle.inv_thumbnail + 
+                     '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + 
+                     ' on CSE Motors" /></a>';
+            grid += '<div class="card-info">'; // Info section for the vehicle
+            grid += '<h2><a href="../../inv/detail/' + vehicle.inv_id + 
+                     '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model + 
+                     ' details">' + vehicle.inv_make + ' ' + vehicle.inv_model + 
+                     '</a></h2>';
+            grid += '<span class="price">$' + 
+                     new Intl.NumberFormat('en-US').format(vehicle.inv_price) + 
+                     '</span>';
+            grid += '</div>'; // End of card-info
+            grid += '</div>'; // End of product-card
+        });
+        grid += '</div>'; // End of inv-display
+    } else {
+        grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>';
     }
-    return grid
-  }
+    return grid;
+}
 
 /* **************************************
  * Build the classification view HTML
