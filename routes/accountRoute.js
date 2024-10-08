dd8fad2caa3cd465e593208d5e7c2f63e2158e6b
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-10-08 13:15:13 -0500

    Styling login and registration, registration page not showing

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,7 +6,20 @@ const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
 
+/* ***********************
+ * Login View
+ *************************/
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+/* ***********************
+ * Register View
+ *************************/
+router.get("/registration", utilities.handleErrors(accountController.buildRegister));
+
+/* ***********************
+ * Process Registration
+ *************************/
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
 
 router.use((err, req, res, next) => {
     console.error(err.stack);
